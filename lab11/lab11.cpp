bf9cd0be3a8bbc6600c447ae93c8c45fd216b339
Riley Slater <rslater003@student.butte.edu>
2017-02-02 23:13:08 +0000

    Lab1.1 Submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,13 @@
+/*  1. Computer picks a random number between 1 and 10. Set an integer to points equal to 10.
+    2. User makes a random guess and based on that guess the computer either
+        A. Tells the user the number is higher
+        B. Tells the user the number is lower
+        C. Tells the user the number is correct
+    3. If the computer has chosen option A, then the computer will deduct 1 point from int points and output: "The number picked is higher."
+            Check if int points has hit 0, if it has, then reset program after showing output: "You lost."
+            If int points is above 0 then repeat step 2.
+       If the computer has chosen option B, then the computer will deduct 1 point from int points and output: "The number picked is lower."
+            Check if int points has hit 0, if it has, then reset program after showing output: "You lost."
+            If int points is above 0 then repeat step 2.
+       If the computer has chosen option C, then the computer will output: "You have guessed my number!" Show congratulations pop-up.
+*/
