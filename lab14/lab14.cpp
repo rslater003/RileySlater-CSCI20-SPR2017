7d41e46aed86b76079d20f600176655f417b22ee
Riley Slater <rslater003@student.butte.edu>
2017-02-08 00:27:54 +0000

    Lab1.4 Submission

--- lab14/lab14.cpp
+++ lab14/lab14.cpp
@@ -9,10 +9,11 @@ int main() {
     int penny = 1;
     int includeHalfDollars;
     int prioritize;
-    cout << "Which Type of Coin would you like to prioritize (0 for Half-Dollars, 1 for Quarters, 2 for Dimes, 3 for Nickels, 4 for Pennies) ";
+    cout << "Which Type of Coin would you like to exclude (0 for Half-Dollars. 1 for Quarters and Half Dollars. 2 for Dimes, Quarters, and Half Dollars. 3 for Nickels, Dimes, Quarters, and Half Dollars. 4 for Just Pennies.) ";
     cin >> prioritize;
     cout << "How much change do you have? (As a Whole Number) ";
     cin >> amountOfChange;
+    int totalAmount = amountOfChange;
     if (prioritize == 0) {                                      //If User Prioritizes Half Dollars
         cout << amountOfChange/50 << " Half-Dollars" << endl;
         amountOfChange = amountOfChange % 50;
@@ -48,5 +49,7 @@ int main() {
     if (prioritize == 4) {                                      //If User Prioritizes Pennies
         cout << amountOfChange/1 << " Pennies" <<endl;
     }
-
+    totalAmount = (totalAmount * .891);
+    cout << "Total Earned: $" << static_cast<double>(totalAmount) << endl;
+    
 }                                                               //ends int main() {
