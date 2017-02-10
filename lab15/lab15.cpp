d83e878de24d6c044da66fc9f6ad352f8a27b018
Riley Slater <rslater003@student.butte.edu>
2017-02-10 00:17:41 +0000

    Lab1.5 Submission

---
+++ lab15/lab15.cpp
@@ -0,0 +1,94 @@
+//Riley Slater
+//February 9, 2017
+//19 Variables placed in specific places in an output in a MadLib.
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string adjective1 = "";
+    string adjective2 = "";
+    string noun1 = "";
+    string noun2 = "";
+    string pluralNoun1 = "";
+    string game1 = "";
+    string pluralNoun2 = "";
+    string verbEndingInING1 = "";
+    string verbEndingInING2 = "";
+    string pluralNoun3 = "";
+    string verbEndingInING3 = "";
+    string noun3 = "";
+    string plant1 = "";
+    string partOfTheBody1 = "";
+    string aPlace1 = "";
+    string verbEndingInING4 = "";
+    string adjective3 = "";
+    string number1 = "";
+    string pluralNoun4 = "";
+    
+    cout << "An Adjective ";
+    cin >> adjective1;
+    cout << endl;     
+    cout << "An Adjective ";   
+    cin >> adjective2;
+    cout << endl;     
+    cout << "A Noun ";   
+    cin >> noun1;
+    cout << endl;       
+    cout << "A Noun "; 
+    cin >> noun2;
+    cout << endl;       
+    cout << "An Plural Noun "; 
+    cin >> pluralNoun1;
+    cout << endl;     
+    cout << "A Game ";   
+    cin >> game1;
+    cout << endl;      
+    cout << "A Plural Noun ";  
+    cin >> pluralNoun2;
+    cout << endl;    
+    cout << "A Verb Ending in 'ing' ";    
+    cin >> verbEndingInING1;
+    cout << endl;     
+    cout << "A Verb Ending in 'ing' ";   
+    cin >> verbEndingInING2;
+    cout << endl;     
+    cout << "A Plural Noun ";   
+    cin >> pluralNoun3;
+    cout << endl;    
+    cout << "A Verb Ending in 'ing' ";    
+    cin >> verbEndingInING3;
+    cout << endl;    
+    cout << "A Noun ";    
+    cin >> noun3;
+    cout << endl;    
+    cout << "A Plant ";    
+    cin >> plant1;
+    cout << endl;    
+    cout << "A Part of The Body ";    
+    cin >> partOfTheBody1;
+    cout << endl;    
+    cout << "A Place ";    
+    cin >> aPlace1;
+    cout << endl;    
+    cout << "A Verb Ending in 'ing' ";    
+    cin >> verbEndingInING4;
+    cout << endl;     
+    cout << "An Adjective ";   
+    cin >> adjective3;
+    cout << endl;     
+    cout << "A Number ";   
+    cin >> number1;
+    cout << endl;    
+    cout << "A Plural Noun ";    
+    cin >> pluralNoun4;
+    cout << endl;
+    
+    cout << "A vacation is when you take a trip to some " << adjective1 << " place with your " << adjective2 << " family." << endl; 
+    cout << "Usually you go to some place that is near a " << noun1 << "or up on a " << noun2 << "." << endl;
+    cout << "A good vacation place is one where you can ride " << pluralNoun1 << " or play a " << game1 << " or go hunting for " << pluralNoun2 << "." << endl;
+    cout << "I like to spend my time " << verbEndingInING1 << " or " << verbEndingInING2 << "." << endl;
+    cout << "When parents go on a vacation, they spend their time eating three " << pluralNoun3 << " a day, and fathers play gold, and mothers sit around " << verbEndingInING3 << "." << endl;
+    cout << "Last summer, my little brother fell in a " << noun3 << " and got poison " << plant1 << " all over his " << partOfTheBody1 << "." << endl;
+    cout << "My family is going to the " << aPlace1 << ", and I will practice " << verbEndingInING4 << "." << endl;
+    cout << "Parents need vacations more than kids because parents are always very " << adjective3 << " and because they have to work " << number1 << "hours every day all yeah making enough " << pluralNoun4 << " to pay for the vacation." << endl;
+}
