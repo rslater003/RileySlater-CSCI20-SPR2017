ab6daf193a5129e327da5bba32c22b09437cd131
Riley Slater <rslater003@student.butte.edu>
2017-02-17 07:21:00 +0000

    Assignment1 Submission

--- Assignment1/Assignment1.cpp
+++ Assignment1/Assignment1.cpp
@@ -7,30 +7,30 @@
 #include <string>
 using namespace std;
 
-struct WageSlip {
+struct WageSlip {    //struct with all the attributes of a wage slip: name, hoursPerWeek, payPerHour
     string name;
     int hoursPerWeek;
     int payPerHour;
 };
 
 int main() {
-    double taxes = .83;
-    int healthInsurance = 130;
+    double taxes = .83;         // type Double so .83 doesn't round down to 0
+    int healthInsurance = 130; 
     double grossPay;
     double netPay;
     WageSlip Weekly;
     
-    cout << "What is your name (First): ";
+    cout << "What is your name (First): ";                  //Normal input and outputs to ask user for name, hoursPerWeek, and payPerHour
     cin >> Weekly.name;
     cout << "How many hours do you work per week: ";
     cin >> Weekly.hoursPerWeek;
     cout << "How much do you make per hour: ";
     cin >> Weekly.payPerHour;
     
-    grossPay = Weekly.payPerHour * Weekly.hoursPerWeek;
-    netPay = (grossPay - healthInsurance) * taxes;
+    grossPay = Weekly.payPerHour * Weekly.hoursPerWeek;     //In order to calculate gross pay we multiply payPerHour by hoursPerWeek
+    netPay = (grossPay - healthInsurance) * taxes;          //In order to calculate net pay we need to subtract healthInsurance from grossPay and then multipy by taxes
     
-    cout << endl << "Wage Slip" << endl;
+    cout << endl << "Wage Slip" << endl;                    //Outputs the complete Wage Slip
     cout << "Name: " << Weekly.name << endl;
     cout << "Hours: " << Weekly.hoursPerWeek << endl;
     cout << "Rate: $" << Weekly.payPerHour << endl;
