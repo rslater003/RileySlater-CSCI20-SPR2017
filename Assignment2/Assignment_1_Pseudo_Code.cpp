b001017b3feb338e7c9d4bd030dfcf0239f4d5d7
Riley Slater <rslater003@student.butte.edu>
2017-03-10 00:11:42 +0000

    Assignment 1 Pseudo Code Submission

---
+++ Assignment2/Assignment_1_Pseudo_Code.cpp
@@ -0,0 +1,20 @@
+//Create a Class called DistanceConverter
+//Create Private Variable for inches
+//Create default constructor that initializes inches to 0
+//Create overloaded constructor that accepts a value for inches then stores it in the private class variable (inches)
+//Create Functions for getting each type of distance (Feet, Inches, Yards, Miles)
+//with declarations of variables in parameters for each value of the type of distance the function is
+//Create Functions for setting each type of distance (Feet, Inches, Yards, Miles)
+//Use Equations for conversions to each different types of distances
+/*
+* Equations are: mile = yards/1760, mile = feet/5280, mile = inches/63360, 
+* yards = miles * 1760, yards = feet/3, yards = inches/36
+* feet = miles * 5280, feet = yards * 3, feet = inches/12
+* inches = miles * 63360, inches = yards * 36, inches = feet * 12
+*/
+/*
+* Create an int main that has inputs for all the program conversions: 1 mile to feet, 
+* 1 mile to inches, 1 mile to meters, 1 mile to yards, 16 feet to miles, 20 yards to feet, 
+* 100 inches to miles, 1 meter to miles.
+*/
+//Create Print function to output all the data based on the object
